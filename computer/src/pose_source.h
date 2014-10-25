94e99dd90e77c6c24405a8b054ca1d8a9f506977
josh <squash111@gmail.com>
2014-10-25 18:46:31 -0400

    josh:Added Angular Motion

--- computer/src/pose_source.h
+++ computer/src/pose_source.h
@@ -7,7 +7,8 @@ namespace pose_source {
 //base position is origin
 #define BASE_POSIT 0, 0, 0
 #define BASE_ANGLE 0, 0, 0
-#define ACCELERATION  0.01
+#define LIN_VELOCITY  0.01
+#define ANG_VELOCITY  0.01
 
 class IPoseSource {
 public:
@@ -29,7 +30,9 @@ class FPSPoseSource : public IPoseSource {
 	Eigen::Matrix<double, 3, 1> position;
 	Eigen::Matrix<double, 3, 1> orientation;
 	Eigen::Matrix<double, 3, 1> momentum; //cache directional data
+	Eigen::Matrix<double, 3, 1> rotation; //cache angular data
 	x11::Window* window;	
+	int window_size[2];
 	x11::Display* display;
 
 public:
@@ -40,21 +43,26 @@ public:
 		
 		window  = r.getWindow();	//get the glx window
 		display = r.getDisplay();	//get the display
+		
+		//get the display size (from rendering.h)
+		window_size[0] = 800;
+		window_size[1] = 600;
   }
 
 	//function will return the pose of the user, and will also
-	// check for changes in user state (button presses) if update
+	// check for changes in user state (button presses) if vel_update
   Eigen::Affine3d get_pose() {
-		update_pose();	//call the update
+		vel_update_pose();	//call the vel_update
 		
 		//generate affine transformation from translation and 2 angles
 		Eigen::Affine3d a = Eigen::Translation3d(position) * \
+			Eigen::AngleAxisd(orientation[0], Eigen::Vector3d::UnitX()) * \
 			Eigen::AngleAxisd(orientation[1], Eigen::Vector3d::UnitY()) * \
 			Eigen::AngleAxisd(orientation[2], Eigen::Vector3d::UnitZ());
     return a;	//return the transformation
   }
 
-	void update_pose(){
+	void vel_update_pose(){
 		if(!window){	//make sure window exists
 			//should probably alert somebody
 			return;	//can't do anything useful, exit
@@ -74,35 +82,29 @@ public:
 
 		XEvent event;	//variable to store the event
 		bool done = false;		//catch escape key
-		bool update = false;	//if a key was read, update momentum
+		bool vel_update = false;	//if a key was read, update momentum
+		bool rot_update = false;	//if a key was read, update rotation
 		unsigned int keycode = 0;	//storage for the keycode
-		Eigen::Matrix<double, 3, 1> m2; //temporary storage, for updates
+		Eigen::Matrix<double, 3, 1> m2,r2; //temporary storage, for vel_updates
 		m2 << 0, 0, 0;	//new momentum is 0 unless directed otherwise
+		r2 << 0, 0, 0;	//new velocity is 0 unless directed otherwise
 		while(display->NextEvent(event) && !done){	//try to get event
 			switch(event.type){	//switch based on type
 				case MotionNotify: //Mouse is moved, adjust 
-					//adjust orientation about x axis (x-axial motion)
-					// or y axis (y-axial motion), if outside of neutral center
-					// scroll relative to mouse distance from center
-					//TODO: map relative to window direction, scroll if not center
-					if(abs(event.xmotion.x) > 50){	//check if in center
-						orientation[0] += (abs(event.xmotion.x) > 500)? \
-							event.xmotion.x/500.0 : 1.0;	//~6-60 deg/sec
-						//fix angle ranges (might not be needed)
-						if (orientation[0] > 2*M_PI) orientation[0] -= 2*M_PI;
-						if (orientation[0] < 0) orientation[0] += 2*M_PI;
-					}
-					//if moving vertically, bound at upper and lower poles
-					if(event.xmotion.y > 50)	//check if moving upwards
-						orientation[1] = std::min(M_PI, orientation[1] + \
-							(event.xmotion.y > 500)? \
-							event.xmotion.y/500.0 : 1.0);	//~6-60 deg/sec
-				  else if(event.xmotion.y < -50)	//check if moving upwards
-						orientation[1] = std::max(M_PI, orientation[1] + \
-							(event.xmotion.y < -500.0)? \
-							event.xmotion.y/500.0 : 1.0);	//~6-60 deg/sec
+					//if moving horizontally, rotate around y axis
+					if(event.xmotion.x - window_size[0]/2 > window_size[0]*.1)	//check threshold
+						r2[1] = -ANG_VELOCITY;
+					else if(event.xmotion.x - window_size[0]/2 < window_size[0]*-.1)	//check threshold
+						r2[1] = ANG_VELOCITY;
+					
+					//if moving vertically, bound at upper and lower poles, rotate around x axis
+					if(event.xmotion.y - window_size[1]/2 > window_size[1]*.1)	//check threshold
+						r2[0] = -ANG_VELOCITY;
+				  else if(event.xmotion.y - window_size[1]/2 < window_size[1]*-.1)	//check threshold
+						r2[0] = ANG_VELOCITY;
+					rot_update = true;	//there was an update
 					/*printf("Caputred Motion Event: (%d, %d)\n", \
-						event.xmotion.x, event.xmotion.y);	//DEBUG */
+						event.xmotion.x, event.xmotion.y);	//DEBUG*/
 					break;
 					
 				case KeyPress: //up/down/left/right button press
@@ -111,31 +113,39 @@ public:
 					if(keycode == XK_Escape){
 						done=true; 
 						break;
-					}else if (keycode == XK_Up)  m2[2] =  ACCELERATION;	//moving forward
-					else if (keycode == XK_Down) m2[2] = -ACCELERATION;	//moving backward
-					else if (keycode == XK_Left) m2[0] = -ACCELERATION;	//moving left
-					else if (keycode == XK_Right) m2[0] = ACCELERATION;	//moving right
+					}else if (keycode == XK_Up)  m2[2] =  LIN_VELOCITY;	//moving forward
+					else if (keycode == XK_Down) m2[2] = -LIN_VELOCITY;	//moving backward
+					else if (keycode == XK_Left) m2[0] = -LIN_VELOCITY;	//moving left
+					else if (keycode == XK_Right) m2[0] = LIN_VELOCITY;	//moving right
 					//printf("Caputred Key Event: code=%d\n",keycode);	//DEBUG
 					
 				case KeyRelease:	//key released, stop motion
-					update = true;	//on either key action force update
+					vel_update = true;	//on either key action force vel_update
 			}
 			//do something with escape key (Done)?
 		}
 		
-		if(update) momentum = m2;	//TODO: check memory here
+		if(vel_update) momentum = m2;	//TODO: check memory here
+		if(rot_update) rotation = r2;	//TODO: check memory here
+		orientation = orientation + rotation;
 		
-		//update the position based on current momentum
-		//Eigen::AngleAxisd roll(orientation[0], Eigen::Vector3d::UnitX());					
-		//Eigen::AngleAxisd pitch(orientation[1], Eigen::Vector3d::UnitY());					
-		//Eigen::AngleAxisd yaw(orientation[2], Eigen::Vector3d::UnitZ());						
-		//Eigen::Quaternion<double> q = roll * pitch;
-		//position = position + q.matrix() * momentum;	//get the momentum
-		position = position + Eigen::Quaternion<double>::Identity() * momentum;
+		//bound the x-orientatino to the poles
+		if(orientation[0] > M_PI*.5) orientation[0] = M_PI*.5;
+		if(orientation[0] < -M_PI*.5) orientation[0] = -M_PI*.5;
+		//wrap the y-orientaiton in [0,2*PI]
+		if(orientation[1] < 0) orientation[1] += M_PI;
+		if(orientation[1] > 2*M_PI) orientation[1] -= M_PI;
+		
+		//vel_update the position based on current momentum
+		Eigen::AngleAxisd roll(orientation[0], Eigen::Vector3d::UnitX());					
+		Eigen::AngleAxisd pitch(orientation[1], Eigen::Vector3d::UnitY());					
+		Eigen::AngleAxisd yaw(orientation[2], Eigen::Vector3d::UnitZ());						
+		Eigen::Quaternion<double> q = roll * pitch * yaw;
+		position = position + q.matrix() * momentum;	//get the momentum
 	}
 
 	void set_pose(Eigen::Affine3d pose){
-		//TODO: decompose the Affine
+		position = pose.translation();	//get the pose translation
 	}
 };
 
