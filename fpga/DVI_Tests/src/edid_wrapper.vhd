64e690f2c9c87446078a07ebf482dce453d09a9a
Khaled Hassan <khaled.hassan@gmail.com>
2014-11-10 16:52:19 -0500

    edid: begin commenting fields in DK2 EDID data (not finished yet)

--- fpga/DVI_Tests/src/edid_wrapper.vhd
+++ fpga/DVI_Tests/src/edid_wrapper.vhd
@@ -17,10 +17,10 @@ architecture RTL of edid_wrapper is
 
     constant EDID_LENGTH : natural := 256;
 
- 
     signal edid_index : std_logic_vector(7 downto 0) := (others => '0');
     type edid_t is array (0 to EDID_LENGTH - 1) of std_logic_vector(7 downto 0);
     signal edid_data : edid_t := (      -- xxd -c1 ~/repos/visar/fpga/DK2.edid | cut -c10-12
+        -- Begin Header --
         x"00",
         x"ff",
         x"ff",
@@ -29,52 +29,67 @@ architecture RTL of edid_wrapper is
         x"ff",
         x"ff",
         x"00",
-        x"3e",
-        x"d2",
-        x"03",
-        x"00",
-        x"00",
-        x"00",
-        x"00",
-        x"00",
-        x"0a",
-        x"18",
-        x"01",
-        x"03",
-        x"80",
-        x"00",
-        x"00",
-        x"78",
-        x"e2",
-        x"60",
-        x"b1",
-        x"aa",
-        x"55",
-        x"40",
-        x"b6",
-        x"23",
-        x"0c",
-        x"50",
-        x"54",
-        x"00",
-        x"00",
-        x"00",
-        x"01",
-        x"01",
-        x"01",
-        x"01",
-        x"01",
-        x"01",
-        x"01",
-        x"01",
-        x"01",
-        x"01",
-        x"01",
-        x"01",
-        x"01",
-        x"01",
-        x"01",
-        x"01",
+        -- End Header --
+        -- Begin Vendor / Product Identification --
+        x"3e",                          -- ID Manufacturer Name --
+        x"d2",                          -- ID Manufacturer Name --
+        x"03",                          -- ID Product Code --
+        x"00",                          -- ID Product Code --
+        x"00",                          -- ID Serial Number --
+        x"00",                          -- ID Serial Number --
+        x"00",                          -- ID Serial Number --
+        x"00",                          -- ID Serial Number --
+        x"0a",                          -- Week of Manufacture --
+        x"18",                          -- Year of Manufacture --
+        -- End Vendor / Product Identification --
+        -- Begin EDID Structure Version / Revision --
+        x"01",                          -- Version # --
+        x"03",                          -- Revision # --
+        -- End EDID Structure Version / Revision --
+        --Begin Basic Display Parameters / Features --
+        x"80",                          -- Video Input Definition --
+        x"00",                          -- Max. Horizontal Image Size --
+        x"00",                          -- Max Vertical Image Size --
+        x"78",                          -- Display Transfer Characteristic (Gamma) --
+        x"e2",                          -- Feature Support --
+        -- End Basic Display Parameters / Features --
+        -- Begin Color Characteristics --
+        x"60",                          -- Red/Green Low Bits --
+        x"b1",                          -- Blue/White Low Bits --
+        x"aa",                          -- Red-x --
+        x"55",                          -- Red-y --
+        x"40",                          -- Green-x --
+        x"b6",                          -- Green-y --
+        x"23",                          -- Blue-x --
+        x"0c",                          -- Blue-y --
+        x"50",                          -- White-x --
+        x"54",                          -- White-y --
+        -- End Color Characteristics --
+        -- Begin Established Timings --
+        x"00",                          -- Established Timings 1 --
+        x"00",                          -- Established Timings 2 --
+        x"00",                          -- Manufacturer's Reserved Timings --
+        -- End Established Timings --
+        -- Begin Standard Timing Identification --
+        x"01",                          -- Standard Timing Identification #1 --
+        x"01",                          -- Standard Timing Identification #1 --
+        x"01",                          -- Standard Timing Identification #2 --
+        x"01",                          -- Standard Timing Identification #2 --
+        x"01",                          -- Standard Timing Identification #3 --
+        x"01",                          -- Standard Timing Identification #3 --
+        x"01",                          -- Standard Timing Identification #4 --
+        x"01",                          -- Standard Timing Identification #4 --
+        x"01",                          -- Standard Timing Identification #5 --
+        x"01",                          -- Standard Timing Identification #5 --
+        x"01",                          -- Standard Timing Identification #6 --
+        x"01",                          -- Standard Timing Identification #6 --
+        x"01",                          -- Standard Timing Identification #7 --
+        x"01",                          -- Standard Timing Identification #7 --
+        x"01",                          -- Standard Timing Identification #8 --
+        x"01",                          -- Standard Timing Identification #8 --
+        -- End Standard Timing Identification --
+        -- Begin Detailed Timing Descriptions --
+        -- Begin Detailed Timing Description #1 --
         x"72",
         x"40",
         x"38",
@@ -93,24 +108,28 @@ architecture RTL of edid_wrapper is
         x"00",
         x"00",
         x"1a",
+        -- End Detailed Timing Description #1 --
+        -- Begin Detailed Timing Description #2 --
         x"00",
         x"00",
         x"00",
-        x"fc",
+        x"fc", -- String Descriptor (EDID implementation guide section 3.7.6)
         x"00",
-        x"52",
-        x"69",
-        x"66",
-        x"74",
-        x"20",
-        x"44",
-        x"4b",
-        x"32",
-        x"0a",
-        x"20",
-        x"20",
-        x"20",
-        x"20",
+        x"52", -- ASCII R
+        x"69", -- ASCII i
+        x"66", -- ASCII f
+        x"74", -- ASCII t
+        x"20", -- ASCII Space
+        x"44", -- ASCII D
+        x"4b", -- ASCII K
+        x"32", -- ASCII 2
+        x"0a", -- ASCII Line Feed
+        x"20", -- ASCII Space (required by specification to fill unused slots with space)
+        x"20", -- ASCII Space
+        x"20", -- ASCII Space
+        x"20", -- ASCII Space
+        -- End Detailed Timing Description #2 --
+        -- Begin Detailed Timing Description #3 --
         x"00",
         x"00",
         x"00",
@@ -129,15 +148,17 @@ architecture RTL of edid_wrapper is
         x"43",
         x"34",
         x"33",
+        -- End Detailed Timing Description #3 --
+        -- Begin Detailed Timing Description #4 --
         x"00",
         x"00",
         x"00",
-        x"fd",
-        x"00",
-        x"38",
-        x"4d",
-        x"1e",
-        x"96",
+        x"fd", -- Range Limits Descriptor (EDID implementation guide section 3.7.5)
+        x"00", -- Minimum vertical field rate (Hz) --
+        x"38", -- Maximum vertical field rate (Hz) --
+        x"4d", -- Minimum horizontal field rate (Hz) --
+        x"1e", -- Maximum horizontal field rate (Hz) --
+        x"96", -- Maximum pixel clock rate x 10 MHz --
         x"11",
         x"00",
         x"0a",
@@ -147,8 +168,10 @@ architecture RTL of edid_wrapper is
         x"20",
         x"20",
         x"20",
-        x"01",
-        x"45",
+        -- End Detailed Timing Description #4 --
+        x"01", -- Extension Flag, 01 means 1 extension to follow --
+        x"45", -- Checksum --
+        -- Begin CEA EDID Timing Extension Version 3 block --
         x"02",
         x"03",
         x"04",
