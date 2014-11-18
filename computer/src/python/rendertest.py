7248f3d19d148f2946bc71cda0265ae9dea20945
josh <squash111@gmail.com>
2014-11-18 17:15:17 -0500

    Moved debug to rendertest

--- computer/src/python/rendertest.py
+++ computer/src/python/rendertest.py
@@ -1,16 +1,37 @@
 # Debug and rendering test for rendering.py  
+# imports for drawable classes
 import pygame
-import rendering        
+import rendering       
+# imports for main program execution 
+import controller
+import user_pose
 
 # wrapper to simulate main function        
 def debug_wrapper():
-  test_renderer = rendering.Renderer() # create the renderer
-  test_drawable1 = Debug_Static() # get object
-  test_renderer.add_module(test_drawable1.draw) # add to render stack
-  test_drawable2 = Debug_Dynamic()
-  test_renderer.add_module(test_drawable2.draw)
-  test_renderer.do_loop() # start renderer loop
-
+  # initialize things
+  pose_source = user_pose.FPS_Pose(two_d=True) # initialize position source
+  visar_controller = controller.Controller(pose_source) # initialize controller
+  renderer = rendering.Renderer(visar_controller) # initialize renderer
+  
+  # add pose source to update stack
+  visar_controller.add_update(pose_source.update) 
+  
+  # setup a static square object (from rendertest)
+  square1 = rendertest.Debug_Static() # create a render module
+  renderer.add_2d_module(square1.draw) # add module's draw to render stack
+  
+  # setup dynamic (moving) square object (rendertest)
+  square2 = rendertest.Debug_Dynamic() # create another module
+  visar_controller.add_update(square2.update) # add pose listener
+  renderer.add_2d_module(square2.draw) # add draw method to render stack
+  
+  # setup a static square object with depth (from rendertest)
+  square3 = rendertest.Debug_Depth() # create a render module
+  renderer.add_3d_module(square3.draw) # add module's draw to render stack
+  
+  #run the renderer
+  renderer.do_loop()
+  
 # test class will draw a red rectangle in a dynamic location
 class Debug_Dynamic:
   def __init__(self, location=(100,150), size=(100,50)):
--- computer/src/python/visar.py
+++ computer/src/python/visar.py
@@ -8,30 +8,16 @@ import menu_button
 
 def visar():
   # initialize things
-  pose_source = user_pose.FPS_Pose(two_d=True) # initialize position source
+  pose_source = user_pose.FPS_Pose() # initialize position source
   visar_controller = controller.Controller(pose_source) # initialize controller
   renderer = rendering.Renderer(visar_controller) # initialize renderer
   
   # add pose source to update stack
   visar_controller.add_update(pose_source.update) 
   
-  # setup a static square object (from rendertest)
-  square1 = rendertest.Debug_Static() # create a render module
-  renderer.add_2d_module(square1.draw) # add module's draw to render stack
-  
-  # setup dynamic (moving) square object (rendertest)
-  square2 = rendertest.Debug_Dynamic() # create another module
-  visar_controller.add_update(square2.update) # add pose listener
-  renderer.add_2d_module(square2.draw) # add draw method to render stack
-  
-  # setup a static square object with depth (from rendertest)
-  square3 = rendertest.Debug_Depth() # create a render module
-  renderer.add_3d_module(square3.draw) # add module's draw to render stack
-  
-
   # menu buttons 
-  # button1 = menu_button.Menu()
-  # renderer.add_module(button1.draw)
+  button1 = menu_button.Menu()
+  renderer.add_module(button1.draw)
   
   # run the renderer
   renderer.do_loop()
