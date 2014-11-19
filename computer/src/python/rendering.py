Correct-ish depth values, proper calculation

computer/src/python/rendering.py
@@ -1,10 +1,19 @@
 # define the class that handles the rendering
 # individual modules should pass it an object with
 #   a draw(surface) method which will return a surface or a Surface_3D
+# Surface depth factor (f*b/2*Pxmax*cot(FOV/2)) is also stored in renderer
 import pygame
 from pygame.locals import *
+import numpy as np
 
 FPS = 30 # run at 30FPS or so
+
+# 3D transformation constants defined here
+EYE_DISTANCE = .06 # (might be dynamic later? 60mm)
+FOCAL_LENGTH = .022 # internet says 22mm for human eyes, confirm later
+FOV_DEGREES = 100.0 # display FOV in degrees
+FOV_RADIANS = FOV_DEGREES / 180.0 * np.pi # convert to radians
+HUD_DEPTH = 3.0 # hud is located at 3m
 
 class Renderer:
   def __init__(self, controller=None, debug=False):
@@ -20,6 +29,9 @@ class Renderer:
     else: self.eye_surface.fill((0,0,0)) # fill with zero
     self.clock = pygame.time.Clock() # timer for fpsing
     self.debug_mode = debug # debug mode only displays left eye
+    # define the depth_factor and HUD_offset amount and store in class
+    self.depth_factor = (FOCAL_LENGTH * EYE_DISTANCE) / (2.0 * np.tan(FOV_RADIANS/2.0)) * self.eye_size[0]
+    self.HUD_offset = int(self.depth_factor / (HUD_DEPTH*HUD_DEPTH) / 2.0)
 
   # method contains a loop running at 30hz
   def do_loop(self):
@@ -45,15 +57,17 @@ class Renderer:
       renders = map(self.draw_3d_map,self.draws_3d) # call the draws
       for img in renders:
         # combine the images for each eye
-        left_eye.blit(img.left_eye_surface(),(0,0))
-        right_eye.blit(img.right_eye_surface(), (0,0))
+        left, right = img.eye_surfaces(self.depth_factor)
+        left_eye.blit(left,(0,0))
+        right_eye.blit(right, (0,0))
       
-      # get and combine each 2d modules's drawn output (draw on top of 3d)
+      # get and combine each 2d modules's drawn output, will be drawn at focal depth
       renders = map(self.draw_2d_map,self.draws_2d) # call the draws
       for img in renders:
         # combine the images (both eyes are equal)
-        left_eye.blit(img,(0,0))
-        right_eye.blit(img,(0,0))
+        left, right = self.eye_surfaces_2d(img)
+        left_eye.blit(left,(0,0))
+        right_eye.blit(right,(0,0))
 
       # debug mode only shows one eye, with no distortion
       if(self.debug_mode):
@@ -91,34 +105,31 @@ class Renderer:
     
   def add_3d_module(self, module_draw):
     self.draws_3d.append(module_draw)
-    
-
-
-# 3D transformations defined here
-EYE_DISTANCE = .06 # (might be dynamic later? 60cm)
-FOCAL_LENGTH = 20 # Literally no idea, get from somebody, assume 20m
+  
+  # function shifts images to HUD_offset  
+  def eye_surfaces_2d(self, img):
+    left = img.copy()
+    right = img.copy()
+    left.scroll(dx=self.HUD_offset)   # move right, dx 
+    right.scroll(dx=-self.HUD_offset) # move left, -dx
+    return left, right
 
 # class for wrapping around 3d surfaces
 class Surface_3D:
   def __init__(self, surface, depth=0):  
     self.surface = surface
-
-    # derived from stereo-vision distance equation
-    # Z = f*b/d -> d = f*b/Z, d = 2*shift amount
-    if(depth < .001): self.shift_amt = 0; # don't divide by zero, or overdo it
-    else: self.shift_amt = int(FOCAL_LENGTH * EYE_DISTANCE / depth / 2)
-  
-  # return the surface for the left eye (move left, -dx)
-  def left_eye_surface(self):
+    self.depth = depth
+    # bound the minimum depth, don't let things inside hud
+    if(depth < HUD_DEPTH): depth = HUD_DEPTH
+      
+  # return the surface for both eyes
+  def eye_surfaces(self, depth_factor):
     left = self.surface.copy()
-    left.scroll(dx=self.shift_amt)
-    return left
-    
-  # return the surface for the right eye (move right, dx)
-  def right_eye_surface(self):
     right = self.surface.copy()
-    right.scroll(dx=-self.shift_amt)
-    return right
+    shift_amt = int(depth_factor / (self.depth*self.depth) / 2.0) # get the shift amount
+    left.scroll(dx=shift_amt)   # move right, dx 
+    right.scroll(dx=-shift_amt) # move left, -dx
+    return left, right
   
 
 