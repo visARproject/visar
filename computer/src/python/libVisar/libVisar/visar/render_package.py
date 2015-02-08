cc8eaba272f0d7110493ceb2fec6d7a786f7de6a
Jacob Panikulam <jpanikulam@ufl.edu>
2015-02-08 17:51:11 -0500

    Create functioning python package

--- computer/src/python/libVisar/libVisar/visar/render_package.py
+++ computer/src/python/libVisar/libVisar/visar/render_package.py
@@ -1,6 +1,6 @@
 from ..OpenGL.utils.transformations import quaternion_matrix
 
-if __name__ == '__main__':
+def main():
     ex_quat = {"x": 0.50155109,  "y": 0.03353513,  "z": 0.05767266, "w": 0.86255189}
     orientation_quaternion = (0.50155109, 0.03353513, 0.05767266, 0.86255189)
     matrix =  quaternion_matrix(orientation_quaternion)
@@ -12,3 +12,5 @@ if __name__ == '__main__':
     ''' The plan here is to contruct a new view matrix for the targets each frame
     As for the map and other UI elements, they don't need a view matrix
     '''
+
+    
--- computer/src/python/libVisar/setup.py
+++ computer/src/python/libVisar/setup.py
@@ -1,18 +1,21 @@
-from distutils.core import setup
-setup(name='jOpenGL',
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='libVisar',
     version='1.0',
     description='Python Oculus Rift DK2 Driver',
     author='Jacob Panikulam',
     author_email='jpanikulam@ufl.edu',
     url='https://www.python.org/',
-    #entry_points={
-    #    "console_scripts": ["ds4drv=ds4drv.__main__:main"]
-    #},
-    package_dir = {
-    	'libVisar': '.',
-
+    entry_points={
+       "console_scripts": ["vsr=libVisar.visar.render_package:main"]
+    },
+    package_dir={
+    	'': '.',
     },
     packages=[
+        'libVisar',
     	'libVisar.OpenGL', 'libVisar.OpenGL.shaders', 'libVisar.OpenGL.rift_parameters',
         'libVisar.visar', 'libVisar.visar.drawables',
     ],
