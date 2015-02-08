12f6f470f5d7f2bd8c68d7f4920285c1186c8669
Jacob Panikulam <jpanikulam@ufl.edu>
2015-02-08 17:26:52 -0500

    Update shaders subpackage

--- computer/src/python/libVisar/OpenGL/shaders/dk2_shaders.py
+++ computer/src/python/libVisar/OpenGL/shaders/dk2_shaders.py
@@ -81,3 +81,48 @@ OculusWarpFrag = """
       gl_FragColor.a = fade;
     }
 """
+
+HLSL_vert = '''Example vert_shader
+
+float2 EyeToSourceUVScale, EyeToSourceUVOffset;
+float4x4 EyeRotationStart, EyeRotationEnd;
+float2 TimewarpTexCoord(float2 TexCoord, float4x4 rotMat)
+{
+    // Vertex inputs are in TanEyeAngle space for the R,G,B channels (i.e. after chromatic
+    // aberration and distortion). These are now "real world" vectors in direction (x,y,1)
+    // relative to the eye of the HMD. Apply the 3x3 timewarp rotation to these vectors.
+    float3 transformed = float3( mul ( rotMat, float4(TexCoord.xy, 1, 1) ).xyz);
+    // Project them back onto the Z=1 plane of the rendered images.
+    float2 flattened = (transformed.xy / transformed.z);
+    // Scale them into ([0,0.5],[0,1]) or ([0.5,0],[0,1]) UV lookup space (depending on eye)
+    return(EyeToSourceUVScale * flattened + EyeToSourceUVOffset);
+}
+void main(in float2 Position : POSITION, in float timewarpLerpFactor : POSITION1,
+in float Vignette : POSITION2, in float2 TexCoord0 : TEXCOORD0,
+in float2 TexCoord1 : TEXCOORD1, in float2 TexCoord2 : TEXCOORD2,
+out float4 oPosition : SV_Position, out float2 oTexCoord0 : TEXCOORD0,
+out float2 oTexCoord1 : TEXCOORD1, out float2 oTexCoord2 : TEXCOORD2,
+out float oVignette : TEXCOORD3)
+{
+    float4x4 lerpedEyeRot = lerp(EyeRotationStart, EyeRotationEnd, timewarpLerpFactor);
+    oTexCoord0 = TimewarpTexCoord(TexCoord0,lerpedEyeRot);
+    oTexCoord1 = TimewarpTexCoord(TexCoord1,lerpedEyeRot);
+    oTexCoord2 = TimewarpTexCoord(TexCoord2,lerpedEyeRot);
+    oPosition = float4(Position.xy, 0.5, 1.0);
+}
+'''
+HLSL_frag = ''' Example frag_shader
+
+Texture2D Texture : register(t0);
+SamplerState Linear : register(s0);
+float4 main(in float4 oPosition : SV_Position, in float2 oTexCoord0 : TEXCOORD0,
+in float2 oTexCoord1 : TEXCOORD1, in float2 oTexCoord2 : TEXCOORD2,
+in float oVignette : TEXCOORD3) : SV_Target
+{
+    // 3 samples for fixing chromatic aberrations
+    float R = Texture.Sample(Linear, oTexCoord0.xy).r;
+    float G = Texture.Sample(Linear, oTexCoord1.xy).g;
+    float B = Texture.Sample(Linear, oTexCoord2.xy).b;
+    return (oVignette*float4(R,G,B,1));
+}
+'''
--- computer/src/python/libVisar/OpenGL/shaders/make_distortion.py
+++ computer/src/python/libVisar/OpenGL/shaders/make_distortion.py
@@ -5,51 +5,7 @@ from vispy.gloo import (Program, VertexBuffer, IndexBuffer, Texture2D, clear,
                         FrameBuffer)
 
 from ..rift_parameters import read_mesh_txt
-'''Example vert_shader
 
-float2 EyeToSourceUVScale, EyeToSourceUVOffset;
-float4x4 EyeRotationStart, EyeRotationEnd;
-float2 TimewarpTexCoord(float2 TexCoord, float4x4 rotMat)
-{
-// Vertex inputs are in TanEyeAngle space for the R,G,B channels (i.e. after chromatic
-// aberration and distortion). These are now "real world" vectors in direction (x,y,1)
-// relative to the eye of the HMD. Apply the 3x3 timewarp rotation to these vectors.
-float3 transformed = float3( mul ( rotMat, float4(TexCoord.xy, 1, 1) ).xyz);
-// Project them back onto the Z=1 plane of the rendered images.
-float2 flattened = (transformed.xy / transformed.z);
-// Scale them into ([0,0.5],[0,1]) or ([0.5,0],[0,1]) UV lookup space (depending on eye)
-return(EyeToSourceUVScale * flattened + EyeToSourceUVOffset);
-}
-void main(in float2 Position : POSITION, in float timewarpLerpFactor : POSITION1,
-in float Vignette : POSITION2, in float2 TexCoord0 : TEXCOORD0,
-in float2 TexCoord1 : TEXCOORD1, in float2 TexCoord2 : TEXCOORD2,
-out float4 oPosition : SV_Position, out float2 oTexCoord0 : TEXCOORD0,
-out float2 oTexCoord1 : TEXCOORD1, out float2 oTexCoord2 : TEXCOORD2,
-out float oVignette : TEXCOORD3)
-{
-float4x4 lerpedEyeRot = lerp(EyeRotationStart, EyeRotationEnd, timewarpLerpFactor);
-oTexCoord0 = TimewarpTexCoord(TexCoord0,lerpedEyeRot);
-oTexCoord1 = TimewarpTexCoord(TexCoord1,lerpedEyeRot);
-oTexCoord2 = TimewarpTexCoord(TexCoord2,lerpedEyeRot);
-oPosition = float4(Position.xy, 0.5, 1.0);
-o
-'''
-
-''' Example frag_shader
-
-Texture2D Texture : register(t0);
-SamplerState Linear : register(s0);
-float4 main(in float4 oPosition : SV_Position, in float2 oTexCoord0 : TEXCOORD0,
-in float2 oTexCoord1 : TEXCOORD1, in float2 oTexCoord2 : TEXCOORD2,
-in float oVignette : TEXCOORD3) : SV_Target
-{
-// 3 samples for fixing chromatic aberrations
-float R = Texture.Sample(Linear, oTexCoord0.xy).r;
-float G = Texture.Sample(Linear, oTexCoord1.xy).g;
-float B = Texture.Sample(Linear, oTexCoord2.xy).b;
-return (oVignette*float4(R,G,B,1));
-}
-'''
 
 class Mesh(object):
     ''' Expect
@@ -150,6 +106,13 @@ class Mesh(object):
 
     @classmethod
     def make_eye(self, texture, eye):
+        '''make_eye (eye_texture, eye)
+        Arguments:
+            - eye_texture: The texture (bound to a framebuffer), that represents the view of the eye
+            - eye: 'left' or 'right', the eye being rendered
+        Todo:
+            - Use vertex buffer instead of manually binding
+        '''
         assert isinstance(texture, Texture2D), "texture not a texture 2D instance!"
         assert eye in ['left', 'right'], eye + " is not a valid eye (Should be left or right)"
 
@@ -159,21 +122,15 @@ class Mesh(object):
         i_buffer = self._i_buffers[eye + '_indices']
         _buffer = self._v_buffers[eye + '_buffer']
 
-        print 'Loading pos -> GPU memory'
+        print 'Loading {} eye distortion mesh pos'.format(eye)
         program['pos'] = _buffer['pos']
-        print 'Loading red_xy -> GPU memory'
+        print 'Loading {} eye distortion mesh red_xy'.format(eye)
         program['red_xy'] = _buffer['red_xy']
-        print 'Loading green_xy -> GPU memory'
+        print 'Loading {} eye distortion mesh green_xy'.format(eye)
         program['green_xy'] = _buffer['green_xy']
-        print 'Loading blue_xy -> GPU memory'
+        print 'Loading {} eye distortion mesh blue_xy'.format(eye)
         program['blue_xy'] = _buffer['blue_xy']
         program['vignette'] = _buffer['vignette']
         program['texture'] = texture
 
-        print _buffer['vignette']
-
         return program, IndexBuffer(i_buffer)
-
-    @classmethod
-    def make_right_eye(self, texture):
-        return NotImplemented
