25188ba790666e64d1fc843ef6d4209d819f5307
Mike Seese <seesemichaelj@gmail.com>
2015-03-09 23:20:37 -0400

    Added a timer

--- computer/src/c/voice_control.c
+++ computer/src/c/voice_control.c
@@ -17,12 +17,14 @@ void start_voice(int* fd) {
   char const *hyp; //hypothesis of decoding, i.e. what pocketsphinx thinks was said
   int16 buffer[2048]; //buffer used to store input
   char new_hyp[2048]; //memory for the hypothesis
+  char time_gate_pass = 0;
 
   //initial configuration for pocketsphinx
   cmd_ln_t *config = cmd_ln_init(NULL, ps_args(), TRUE,
     "-hmm", MODELDIR "/en-us/en-us",
     "-lm", MODELDIR "/en-us/en-us.lm.dmp",
     "-dict", MODELDIR "/en-us/cmudict-en-us.dict",
+    "-logfn", "/dev/null",
     NULL);
 
   //setup decoder
@@ -30,6 +32,10 @@ void start_voice(int* fd) {
 
   //begin utterance
   ps_start_utt(ps);
+
+  time_t start_time_in_speech;
+  time_t current_time;
+  struct tm * timeinfo;
 
   //go as long as there's a pipe
   while((k = read(fd[0], buffer, 2048)) > 0) {
@@ -42,10 +48,19 @@ void start_voice(int* fd) {
     //when speech is introduced in each new utterance
     if(in_speech && !utt_started) {
       utt_started = TRUE;
+      time(&start_time_in_speech);
+      printf("Listening...\n");
+    }
+    else if(utt_started) {
+      //printf("Time elapsed: %d\n", time(0) - start_time_in_speech );
+      if( time(0) - start_time_in_speech > 5 ) {
+        time_gate_pass = 1;
+        printf("No longer listening.\n");
+      }
     }
 
     //once speech is done and utterance has started
-    if(!in_speech && utt_started) {
+    if(!in_speech && utt_started && time_gate_pass) {
       ps_end_utt(ps); //end the utterance
       hyp = ps_get_hyp(ps, NULL); //get the hypothesis
       memcpy(new_hyp, hyp, 2048); //transfer hyp to prevent corruption
@@ -67,9 +82,10 @@ void start_voice(int* fd) {
       write(fd[1], sentence, strlen(sentence)); //send results to output pipe
 
       utt_started = FALSE; //reset check back to FALSE
+      time_gate_pass = 0;
     }
 
-    // usleep(7500);
+    //usleep(100000);
     memset(buffer, 0, 2048);
   }
 
