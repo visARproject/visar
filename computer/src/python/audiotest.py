6bb71ac4f0f3716fb47c4cf2f95c650d6bdf16eb
josh <squash111@gmail.com>
2015-03-10 20:10:41 -0400

    Audio modules and tests finished

---
+++ computer/src/python/audiotest.py
@@ -0,0 +1,46 @@
+import audio
+import interface
+import time
+
+def audioCallback(event1, event2):
+  print event1, event2
+  
+class voice_event(interface.Interface):
+  def __init__(self):
+    interface.Interface.__init__(self)
+    
+def voiceCallback(event):
+  print event
+  
+def test():
+  print 'Starting Audio'
+  audio_state = audio.AudioController()
+  audio_state.add_callback(audioCallback)
+  
+  time.sleep(1)
+  print 'starting audio'
+  audio_state.start('127.0.0.1')
+  
+  time.sleep(3)
+  print 'starting vc'
+  voice = voice_event()
+  voice.add_callback(voiceCallback)
+  audio_state.start_voice(voice)
+  
+  time.sleep(3)
+  print 'stopping vc'
+  audio_state.stop_voice()
+  
+  time.sleep(3)
+  print 'stopping audio'
+  audio_state.stop()
+  
+  time.sleep(1)
+  print 'cleaning up...'
+  audio_state.destroy()
+  
+  time.sleep(2)
+  print 'Done'
+  
+if __name__ == '__main__':
+  test()
