45416ba8760d8db5d66aca159f95a9b26ce7a62b
Jacob Panikulam <jpanikulam@ufl.edu>
2015-03-12 00:12:41 -0400

    Rendering: Add color to logger

--- computer/src/python/libVisar/libVisar/OpenGL/utils/logger.py
+++ computer/src/python/libVisar/libVisar/OpenGL/utils/logger.py
@@ -1,11 +1,24 @@
+from colorama import Fore, Back, Style
+import sys
+
 class Logger(object):
-    ''' Verbosity:
+    ''' 
+    Verbosity:
         0 -> Do nothing
         1 -> print warnings
         2 -> print warnings and log messages
+
+    Colorama Examples:
+        print(Fore.RED + 'some red text')
+        print(Back.GREEN + 'and with a green background')
+        print(Style.DIM + 'and in dim text')
+        print(Fore.RESET + Back.RESET + Style.RESET_ALL)
+        print('back to normal now')
+
     '''
     verbosity = 0
     max_verbosity = 2
+    reset_color = Fore.RESET + Back.RESET + Style.RESET_ALL
 
     @classmethod
     def set_verbosity(self, verbosity):
@@ -26,10 +39,10 @@ class Logger(object):
     def log(self, *args):
         if self.verbosity > 1:
             out_str = " ".join([str(i) for i in args])
-            print 'VISAR Log:', out_str
+            print Fore.BLUE + '[VISAR Log]:', out_str + self.reset_color
 
     @classmethod
     def warn(self, *args):
         if self.verbosity > 0:
             out_str = " ".join([str(i) for i in args])
-            print 'VISAR Warning:', out_str
+            print Fore.RED + '[VISAR Warning]:', out_str + self.reset_color
