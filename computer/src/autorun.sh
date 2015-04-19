4f20d67dec26717636470697faa6cf87e04aa47a
josh <squash111@ufl.edu>
2015-04-19 01:54:42 +0000

    display patch in autorun.sh

--- computer/src/autorun.sh
+++ computer/src/autorun.sh
@@ -1,12 +1,15 @@
 # configure the ad-hoc network, will need to be adjusted on each unit
-#ifconfig wlan0 down
-#iwconfig wlan0 mode ad-hoc
-#iwconfig wlan0 essid 'visar'
-#iwconfig wlan0 key 1234567890
-#ifconfig wlan0 192.168.1.1
+ifconfig wlan0 down
+iwconfig wlan0 mode ad-hoc
+iwconfig wlan0 essid 'visar'
+iwconfig wlan0 key 1234567890
+ifconfig wlan0 192.168.1.14
 
 # rotate the display (todo: confirm that this is needed)
-# DISPLAY=:0.0 xrandr -o left
+DISPLAY=:0.0 xrandr -o left
+
+# Uncomment if program displays on half of the screen, fixes resolution
+#DISPLAY=:0.0 xrandr --output HDMI-0 --mode 1080x1920 --rate 48.0
 
 # start Voice control program in background (should hopefully be bundled)
 #(/home/ubuntu/src/python/libVisar/libVisar/visar/audio/vc &)
@@ -16,4 +19,4 @@ vsr -f
 
 # shtudown the computer when program exits (disabled for testing)
 #shutdown -h now
-echo Insert Shutdown Here
+#echo Insert Shutdown Here
