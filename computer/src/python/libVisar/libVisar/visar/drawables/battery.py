cd8b2da499b3c5b3d65fdd19ec39c406f0a9bfcb
Valentin Caloean <val0528@yahoo.com>
2015-04-21 11:51:48 -0400

    moved the battery icon

--- computer/src/python/libVisar/libVisar/visar/drawables/battery.py
+++ computer/src/python/libVisar/libVisar/visar/drawables/battery.py
@@ -72,13 +72,13 @@ class Battery(Drawable):
 
         height, width   = 3.0, 6.0
         scale_factor    = 0.2
-        x_offset        = -1.35
-        y_offset        = 4.5
+        x_offset        = -8
+        y_offset        = 2
         pixel_to_length = 10
         color           = (1.0, 1.0, 1.0)
 
         scale(self.model, scale_factor)
-        yrotate(self.model, 60)
+        yrotate(self.model, -60)
         translate(self.model, x_offset, y_offset, -10)
         size = (int(height*100), int(width*100))
 
